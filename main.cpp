d903de2e269e61b8d51394b0fa81bdfb0f571b6a
Marcel Timmerman <mt1957@gmail.com>
2018-07-30 22:43:09 +0200

    used config for tests

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "textload.h"
 
+#include "textload.h"
+#include "config.h"
+
+// ----------------------------------------------------------------------------
 int main( int argc, char *argv[]) {
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
   QGuiApplication app( argc, argv);
-  app.setApplicationVersion("0.5.0");
+  app.setApplicationVersion("0.6.0");
   app.setApplicationDisplayName("HikingCompanion");
 
   qmlRegisterType<TextLoad>( "io.github.martimm.HikingCompanion.textload", 1, 0, "TextLoad");
+  qmlRegisterType<Config>( "io.github.martimm.HikingCompanion.config", 1, 0, "Config");
+
+  Config *c = new Config();
+  qDebug() << "osType: " << c->osType();
 
   QQmlApplicationEngine engine;
   engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
