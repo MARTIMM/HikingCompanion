23d64cf0ac5eff588fc7ac8f4e7db97c65d3e7da
Marcel Timmerman <mt1957@gmail.com>
2018-08-03 22:55:44 +0200

    code layout

--- main.cpp
+++ main.cpp
@@ -12,24 +12,32 @@ int main( int argc, char *argv[]) {
   app.setApplicationVersion("0.6.0");
   app.setApplicationDisplayName("HikingCompanion");
 
-  qmlRegisterType<TextLoad>( "io.github.martimm.HikingCompanion.textload", 0, 1, "TextLoad");
-  qmlRegisterType<Config>( "io.github.martimm.HikingCompanion.config", 0, 1, "Config");
+  qmlRegisterType<TextLoad>(
+        "io.github.martimm.HikingCompanion.textload", 0, 1, "TextLoad"
+        );
+  qmlRegisterType<Config>(
+        "io.github.martimm.HikingCompanion.config", 0, 1, "Config"
+        );
 
   qmlRegisterSingletonType(
-    QUrl("file:////home/marcel/Projects/Mobile/Projects/HikingCompanion/HikingCompanion/Qml/GlobalVariables.qml"),
-    "io.github.martimm.HikingCompanion.GlobalVariables", 0, 1, "GlobalVariables"
-  );
+        QUrl("qrc:/Qml/GlobalVariables.qml"),
+        "io.github.martimm.HikingCompanion.GlobalVariables", 0, 1,
+        "GlobalVariables"
+        );
+
+  qmlRegisterSingletonType(
+        QUrl("qrc:/Qml/Style.qml"),
+        "io.github.martimm.HikingCompanion.Style", 0, 1, "Style"
+        );
 
 // Config *c = new Config();
 // qDebug() << "osType: " << c->osType();
 
   QQmlApplicationEngine engine;
   engine.load(QUrl(QStringLiteral("qrc:/Qml/Main/Application.qml")));
+
 qDebug() << engine.rootObjects();
 
   if ( engine.rootObjects().isEmpty() ) return -1;
-
-  int sts = app.exec();
-  qDebug() << "Sts: " << sts;
-  return sts;
+  return app.exec();
 }
