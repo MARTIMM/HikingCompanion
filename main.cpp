ebeb90036c5fb135d45a875864005d054ed25ff6
Marcel Timmerman <mt1957@gmail.com>
2018-09-01 15:49:29 +0200

    a note added

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main( int argc, char *argv[]) {
 
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
+  // Settings used by QSettings to set a location to store its data
   QCoreApplication::setOrganizationName("martimm");
   QCoreApplication::setOrganizationDomain("io.github.martimm");
   QCoreApplication::setApplicationName("HikingCompanion");
