41982e45334941fd41e28669963bc4b2502567a4
Marcel Timmerman <mt1957@gmail.com>
2018-10-15 19:01:45 +0200

    on android requesting for permissions is needed

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@
 #include "gpxfiles.h"
 #include "hikes.h"
 
-/*
 #if defined(Q_OS_ANDROID)
-#include "jnitest.h"
+#include <QtAndroid>
 #endif
-*/
 
 //#include <QStyleFactory>
 #include <QGuiApplication>
@@ -23,12 +21,25 @@
 // ----------------------------------------------------------------------------
 int main( int argc, char *argv[]) {
 
-/*
 #if defined(Q_OS_ANDROID)
-  JniTest *t = new JniTest();
-  if ( true ) return 0;
+  // On android, we must request the user of the application for the following
+  // permissions to create data and access devices
+  QStringList permissions = {
+    "android.permission.ACCESS_LOCATION_EXTRA_COMMANDS",
+    "android.permission.ACCESS_FINE_LOCATION",
+    "android.permission.ACCESS_COARSE_LOCATION",
+    "android.permission.WRITE_EXTERNAL_STORAGE",
+    "android.permission.READ_EXTERNAL_STORAGE"
+  };
+  QtAndroid::PermissionResultMap rpm = QtAndroid::requestPermissionsSync(
+        permissions
+        );
+
+  QStringList keys = rpm.keys();
+  for ( int rpmi = 0; rpmi < keys.count(); rpmi++) {
+    qDebug() << keys[rpmi] << (rpm[keys[rpmi]] == QtAndroid::PermissionResult::Granted ? "ok" : "denied");
+  }
 #endif
-*/
 
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
@@ -46,55 +57,15 @@ int main( int argc, char *argv[]) {
   app.setApplicationVersion("0.9.1");
   app.setApplicationDisplayName("HikingCompanion");
 
+/*
   qDebug() << "App data location:" << QStandardPaths::standardLocations(QStandardPaths::AppDataLocation);
   qDebug() << "App config location:" << QStandardPaths::standardLocations(QStandardPaths::AppConfigLocation);
   qDebug() << "Generic data location:" << QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation);
-//  qDebug() << "Download location:" << QStandardPaths::standardLocations(QStandardPaths::DownloadLocation);
-//  qDebug() << "Documents location:" << QStandardPaths::standardLocations(QStandardPaths::DocumentsLocation);
-/**/
+  qDebug() << "Generic config location:" << QStandardPaths::standardLocations(QStandardPaths::GenericConfigLocation);
 
   qDebug() << "qApp argc:" << argc;
   for ( int i = 0; i < argc; i++) {
     qDebug() << QString("qApp [%1]").arg(i) << argv[i];
-  }
-
-#if defined(Q_OS_LINUX)
-  // If there is an extra argument, it should be a path to new
-  // hiking information which must be copied to its proper place
-  if ( qApp->arguments().count() == 2 ) {
-    Config *cfg = new Config;
-    cfg->installNewData(QString(qApp->arguments()[1]));
-
-    return 0;
-  }
-#endif
-
-/*
-  // When there is no argument, check if there is some data in a shared
-  // memory segment
-  else {
-    QSharedMemory smForPath(QString("HikingCompanionPath"));
-    if ( smForPath.attach() ) {
-
-      qDebug() << "HC Attached to sm";
-      smForPath.lock();
-
-      QString path(reinterpret_cast<const char *>(smForPath.data()));
-
-      qDebug() << "Got path:" << path;
-
-      smForPath.unlock();
-      smForPath.detach();
-
-      Config *cfg = new Config;
-      cfg->installNewData(path);
-
-      return 0;
-    }
-
-    else {
-      qDebug() << "HC Not attached to sm" << smForPath.errorString();
-    }
   }
 */
 
@@ -143,20 +114,24 @@ int main( int argc, char *argv[]) {
   return app.exec();
 }
 
+// ============================================================================
+extern "C" {
+
 #if defined(Q_OS_ANDROID)
-//#include <QAndroidActivityResultReceiver>
-//#include <QtAndroid>
-//#include <QtAndroidExtras>
+
+#include <QtAndroid>
 #include <QAndroidJniEnvironment>
 
-extern "C" {
+//#include <QAndroidActivityResultReceiver>
+//#include <QtAndroidExtras>
+
+/*
+// ----------------------------------------------------------------------------
 JNIEXPORT void JNICALL Java_utils_Jnitest_main2__Ljava_lang_String_2 (
-     JNIEnv *env,        /* interface pointer */
-     jobject obj,        /* "this" pointer */
-     jstring jpath       /* argument #1 */
+     JNIEnv *env, jobject obj, jstring jpath
      ) {
 
-  /* Obtain a C-copy of the Java string */
+  / * Obtain a C-copy of the Java string * /
   jboolean copy = false;
   const char *path = env->GetStringUTFChars( jpath, &copy);
 
@@ -164,9 +139,43 @@ JNIEXPORT void JNICALL Java_utils_Jnitest_main2__Ljava_lang_String_2 (
   Config *cfg = new Config;
   cfg->installNewData(QString(*path));
 
-  /* Now we are done with str */
+  / * Now we are done with str * /
   env->ReleaseStringUTFChars( jpath, path);
 }
+*/
+/*
+// ----------------------------------------------------------------------------
+JNIEXPORT jstring JNICALL Java_utils_TDAndroidUtils_getDataRootDir_2 (
+     JNIEnv *env,
+     jobject obj
+     ) {
+  Q_UNUSED(env)
+  Q_UNUSED(obj)
+
+  //QString drd = au->dataRootDir();
+  //qDebug() << "Sending" << drd << "to java";
+  //QAndroidJniObject jstr = QAndroidJniObject::fromString(drd);
+  QAndroidJniObject jstr = QAndroidJniObject::fromString("");
+
+  // qtcreator errors on jstr.object<jstring>(). According to the code
+  // a static cast is applied so we do that instead.
+  return static_cast<jstring>(jstr.object());
 }
+*/
+/*
+// ----------------------------------------------------------------------------
+JNIEXPORT void JNICALL Java_utils_TDAndroidUtils_moveDataPublic_2 (
+     JNIEnv *env,
+     jobject obj
+     ) {
+  Q_UNUSED(env)
+  Q_UNUSED(obj)
+
+  //UtilsInterface *ui = new UtilsInterface();
+  //ui->installHikingData();
+}
+*/
+
 #endif
 
+} // extern "C"
