992f5409917049fe62db09bb8a9a546785d87e98
Marcel Timmerman <mt1957@gmail.com>
2018-11-06 11:11:46 +0100

    swapping test page

--- main.cpp
+++ main.cpp
@@ -89,9 +89,9 @@ int main( int argc, char *argv[]) {
   app.setFont(QFont("Symbola"));
 
   applicationEngine = new QQmlApplicationEngine();
-//  applicationEngine->load(QUrl(QStringLiteral("qrc:/Qml/Main/Application.qml")));
-  applicationEngine->load(QUrl(QStringLiteral("qrc:/Assets/Theme/ThemeTest.qml")));
-
+  applicationEngine->load(QUrl(QStringLiteral("qrc:/Qml/Main/Application.qml")));
+//  applicationEngine->load(QUrl(QStringLiteral("qrc:/Assets/Theme/ThemeTest.qml")));
+qDebug() << "Root objects:" << applicationEngine->rootObjects();
   if ( applicationEngine->rootObjects().isEmpty() ) return -1;
 
   return app.exec();
